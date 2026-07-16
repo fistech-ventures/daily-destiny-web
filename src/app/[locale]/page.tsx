7ed6e8e6fb577a53850be4583c40c082487491ad
iftykhar alam <s.m.ifty49@gmail.com>
2026-07-16 14:05:16 +0600

    feat: Add aspect ratio support to AdBanner component for responsive design

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -282,7 +282,7 @@ export default async function Home({
     <main className="container mx-auto px-1.5 py-0 pb-2 flex flex-col">
       {/* ── TOP BANNER ── */}
       <div className="mb-3 lg:mb-5">
-        <AdBanner className="rounded-lg" altText="হোমপেজ বিজ্ঞাপন" />
+        <AdBanner className="rounded-lg" altText="হোমপেজ বিজ্ঞাপন" aspectRatio="6.5/1" />
       </div>
 
       <div className="mb-3 lg:mb-5">
@@ -327,8 +327,9 @@ export default async function Home({
             </div>
             <div className="flex-[1_1_0%]">
               <AdBanner
-                className="rounded-lg h-full"
+                className="rounded-lg"
                 altText="এলাকার সংবাদ বিজ্ঞাপন"
+                aspectRatio="16/9"
               />
             </div>
           </div>
@@ -529,7 +530,7 @@ export default async function Home({
 
       {/* ── ADVERTISEMENT ── */}
       <div className="mb-3 lg:mb-5">
-        <AdBanner className="rounded-lg" altText="মাঝপাতার বিজ্ঞাপন" />
+        <AdBanner className="rounded-lg" altText="মাঝপাতার বিজ্ঞাপন" aspectRatio="6.5/1" />
       </div>
 
       {/* ── ENTERTAINMENT, SPORTS & EDUCATION ── */}
